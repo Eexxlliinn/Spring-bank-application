fb85a4d55e98d135375b602a868d738db7726ea6
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-22 13:00:33 +0300

    commit

--- src/main/java/com/example/springbankapplication/entities/Client.java
+++ src/main/java/com/example/springbankapplication/entities/Client.java
@@ -1,14 +1,52 @@
 package com.example.springbankapplication.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@Table(name = "Clients")
 public class Client {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "fullName")
+    private String fullName;
+
+    @Column(name = "age")
+    private String age;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<CreditCard> creditCards = new ArrayList<>();
+
+    public Client(String fullName, String age, String address, String phoneNumber, User user) {
+        this.fullName = fullName;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.user = user;
+    }
+
+    public void addCreditCard(CreditCard creditCard) {
+        this.creditCards.add(creditCard);
+    }
 
 }
