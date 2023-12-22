8ab9d1c8f6de0624f0bdcd3a187261ecaaeef649
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-22 13:00:57 +0300

    commit

--- src/main/java/com/example/springbankapplication/repositories/ClientRepository.java
+++ src/main/java/com/example/springbankapplication/repositories/ClientRepository.java
@@ -1,9 +1,12 @@
 package com.example.springbankapplication.repositories;
 
 import com.example.springbankapplication.entities.Client;
+import com.example.springbankapplication.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
+
+    Client findByUser(User user);
 }
