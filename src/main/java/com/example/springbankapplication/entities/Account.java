9e385f65c7ca3b1a232766c1c77c6b543ad05e3d
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-22 12:59:50 +0300

    commit

--- src/main/java/com/example/springbankapplication/entities/Account.java
+++ src/main/java/com/example/springbankapplication/entities/Account.java
@@ -1,13 +1,43 @@
 package com.example.springbankapplication.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
+@Table(name = "Accounts")
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@RequiredArgsConstructor
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "accountNumber")
+    private final String accountNumber;
+
+    @Column(name = "moneyAmount")
+    private String moneyAmount;
+
+    @Column(name = "currency")
+    private final String currency;
+
+    @OneToOne
+    @JoinColumn(name = "creditCard", referencedColumnName = "id")
+    private CreditCard creditCard;
+
+    @ManyToOne
+    @JoinColumn(name = "client", referencedColumnName = "id")
+    private final Client client;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "block")
+    private String block;
 }
