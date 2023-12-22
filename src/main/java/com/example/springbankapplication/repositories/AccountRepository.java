01662f48d7c359825c307f80715587e29e9bd2c9
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-22 12:59:59 +0300

    commit

--- src/main/java/com/example/springbankapplication/repositories/AccountRepository.java
+++ src/main/java/com/example/springbankapplication/repositories/AccountRepository.java
@@ -1,9 +1,17 @@
 package com.example.springbankapplication.repositories;
 
 import com.example.springbankapplication.entities.Account;
+import com.example.springbankapplication.entities.Client;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    List<Account> findByStatus(String status);
+
+    List<Account> findByClient(Client client);
+    List<Account> findByBlock(String block);
 }
