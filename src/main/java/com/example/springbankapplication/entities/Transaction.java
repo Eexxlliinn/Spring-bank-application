161faad37cf49714a023368642defa0c5599f3c2
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-25 21:29:33 +0300

    commit

--- src/main/java/com/example/springbankapplication/entities/Transaction.java
+++ src/main/java/com/example/springbankapplication/entities/Transaction.java
@@ -1,13 +1,45 @@
 package com.example.springbankapplication.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Entity
 @Data
+@Table(name = "Transactions")
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class Transaction {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "creationDate")
+    private Date creationDate;
+
+    @Column(name = "amount")
+    private String amount;
+
+    @ManyToOne
+    @JoinColumn(name = "account", referencedColumnName = "id")
+    private Account account;
+
+    private final String type;
+
+    public Transaction(String description, Date date, String amount, Account account, String type) {
+        this.description = description;
+        this.creationDate = date;
+        this.amount = amount;
+        this.account = account;
+        this.type = type;
+    }
 }
