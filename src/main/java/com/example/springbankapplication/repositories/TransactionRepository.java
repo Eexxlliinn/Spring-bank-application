c89d847a30c011a07886c3678d2f9c996b8d9b04
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-25 21:29:08 +0300

    commit

--- src/main/java/com/example/springbankapplication/repositories/TransactionRepository.java
+++ src/main/java/com/example/springbankapplication/repositories/TransactionRepository.java
@@ -1,9 +1,17 @@
 package com.example.springbankapplication.repositories;
 
+import com.example.springbankapplication.entities.Account;
+import com.example.springbankapplication.entities.Client;
 import com.example.springbankapplication.entities.Transaction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+
+    @Query("SELECT t FROM Transaction t WHERE t.account IN (SELECT a FROM Account a WHERE a.client = ?1)")
+    List<Transaction> findByAccount_CreditCard_Client(Client client);
 }
