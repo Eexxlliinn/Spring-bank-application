7473639fa10dae4f891abf562ebd6f1033d685cf
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-25 21:28:56 +0300

    commit

--- src/main/java/com/example/springbankapplication/repositories/RoleRepository.java
+++ src/main/java/com/example/springbankapplication/repositories/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.example.springbankapplication.repositories;
 
 import com.example.springbankapplication.entities.Role;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
+    @Query("SELECT r FROM Role r WHERE r.roleName = ?1")
+    Role findByRoleName(String roleName);
 }
