257d78071d4bbebe4d5db1c0cb36fc02daddb3f5
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-12-25 21:27:46 +0300

    commit

--- src/main/java/com/example/springbankapplication/repositories/CreditCardRepository.java
+++ src/main/java/com/example/springbankapplication/repositories/CreditCardRepository.java
@@ -1,9 +1,14 @@
 package com.example.springbankapplication.repositories;
 
+import com.example.springbankapplication.entities.Client;
 import com.example.springbankapplication.entities.CreditCard;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CreditCardRepository extends CrudRepository<CreditCard, Long> {
+    List<CreditCard> findByClient(Client client);
+
 }
